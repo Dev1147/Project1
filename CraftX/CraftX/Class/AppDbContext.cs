538acdb1b75427f8a14188d2dc7813f13fb4e7f8
Dev1147 <kkang47.ixii@gmail.com>
2024-06-18 23:21:03 +0900

    프로시저 함수 추가

--- CraftX/CraftX/Class/AppDbContext.cs
+++ CraftX/CraftX/Class/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace CraftX.Class
 {
@@ -18,5 +19,18 @@ namespace CraftX.Class
         public DbSet<InternalMemo> TBL_InternalMemos { get; set; }
 
         public DbSet<DefectRate> TBL_DefectRates { get; set; }
+
+        //이 방법이 더 좋다고 함
+        public async Task<List<T>> CallStoredProcedureAsync2<T>(string procedureName, Dictionary<string, object> parameters) where T : class
+        {
+            var sqlParameters = parameters.Select(p => new SqlParameter(p.Key, p.Value ?? DBNull.Value)).ToArray();
+            var parameterNames = string.Join(", ", parameters.Select(p => p.Key));
+
+            string sqlQuery = $"EXEC {procedureName} {string.Join(", ", sqlParameters.Select(p => p.ParameterName))}";
+
+            return await Set<T>()
+                .FromSqlRaw(sqlQuery, sqlParameters)
+                .ToListAsync();
+        }
     }
 }
